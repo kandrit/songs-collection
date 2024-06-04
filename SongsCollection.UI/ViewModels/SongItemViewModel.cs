eaca709ba3293547dfc36b6844c95a1db1d5db26
Voulimiotis Ioannis <john24g@gmail.com>
2024-06-04 22:00:57 +0300

    Introduced SongItemViewModel to properly update the songs collection listbox

---
+++ SongsCollection.UI/ViewModels/SongItemViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using SongsCollection.Core.Models;
+
+namespace SongsCollection.UI.ViewModels;
+
+public class SongItemViewModel : INotifyPropertyChanged
+{
+    public readonly Song Song;
+
+    public string Title
+    {
+        get => this.Song.Title;
+        set
+        {
+            this.Song.Title = value;
+            OnPropertyChanged(nameof(Title));
+        }
+    }
+
+    public string Artist
+    {
+        get => this.Song.Artist ?? string.Empty;
+        set
+        {
+            this.Song.Artist = value;
+            OnPropertyChanged(nameof(Artist));
+        }
+    }
+
+    public SongItemViewModel(Song song)
+    {
+        this.Song = song;
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public void OnPropertyChanged(string propertyName)
+    {
+        if (this.PropertyChanged != null)
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
--- SongsCollection.UI/ViewModels/SongsEditorViewModel.cs
+++ SongsCollection.UI/ViewModels/SongsEditorViewModel.cs
@@ -5,97 +5,99 @@ using SongsCollection.Core;
 using SongsCollection.Core.Models;
 using SongsCollection.UI.Commands;
 
-namespace SongsCollection.UI.ViewModels
+namespace SongsCollection.UI.ViewModels;
+
+public class SongsEditorViewModel : INotifyPropertyChanged
 {
-    public class SongsEditorViewModel : INotifyPropertyChanged
+    private readonly SongsManager songsManager;
+
+    public ObservableCollection<SongItemViewModel> SongsCollection { get; set; } = [];
+
+    private string currentTitle = string.Empty;
+    public string CurrentTitle
     {
-        private readonly SongsManager songsManager;
-
-        public ObservableCollection<Song> SongsCollection { get; set; } = [];
-
-        private string currentTitle = string.Empty;
-        public string CurrentTitle
+        get => this.currentTitle;
+        set
         {
-            get => this.currentTitle;
-            set
-            {
-                this.currentTitle = value;
-                OnPropertyChanged(nameof(CurrentTitle));
-            }
+            this.currentTitle = value;
+            OnPropertyChanged(nameof(CurrentTitle));
         }
+    }
 
-        private string currentArtist = string.Empty;
-        public string CurrentArtist
+    private string currentArtist = string.Empty;
+    public string CurrentArtist
+    {
+        get => this.currentArtist;
+        set
         {
-            get => this.currentArtist;
-            set
-            {
-                this.currentArtist = value;
-                OnPropertyChanged(nameof(CurrentArtist));
-            }
+            this.currentArtist = value;
+            OnPropertyChanged(nameof(CurrentArtist));
         }
+    }
 
-        private Song? selectedSong;
-        public Song? SelectedSong
+    private SongItemViewModel? selectedSong;
+    public SongItemViewModel? SelectedSong
+    {
+        get => this.selectedSong;
+        set
         {
-            get => this.selectedSong;
-            set
-            {
-                this.selectedSong = value;
-                OnPropertyChanged(nameof(SelectedSong));
-            }
+            this.selectedSong = value;
+            OnPropertyChanged(nameof(SelectedSong));
         }
+    }
 
-        public ICommand SelectedSongChangedCommand => new Command(selectedSong =>
-        {
-            var song = selectedSong as Song;
-            this.CurrentTitle = song?.Title ?? string.Empty;
-            this.CurrentArtist = song?.Artist ?? string.Empty;
-            this.selectedSong = song;
-        }, o => true);
+    public ICommand SelectedSongChangedCommand => new Command(selectedSong =>
+    {
+        var song = selectedSong as SongItemViewModel;
+        this.CurrentTitle = song?.Title ?? string.Empty;
+        this.CurrentArtist = song?.Artist ?? string.Empty;
+        this.selectedSong = song;
+    }, o => true);
 
-        public ICommand AddNewCommand => new Command(o =>
-        {
-            var addedSong = this.songsManager.AddSong(new Song(0, this.CurrentTitle, this.CurrentArtist));
-            this.SongsCollection.Add(addedSong);
-            this.SelectedSong = addedSong;
-        }, o => !string.IsNullOrEmpty(this.currentTitle));
+    public ICommand AddNewCommand => new Command(o =>
+    {
+        var addedSong = this.songsManager.AddSong(new Song(0, this.CurrentTitle, this.CurrentArtist));
+        var addedSongViewModel = new SongItemViewModel(addedSong);
+        this.SongsCollection.Add(addedSongViewModel);
+        this.SelectedSong = addedSongViewModel;
+    }, o => !string.IsNullOrEmpty(this.currentTitle));
 
-        public ICommand UpdateCommand => new Command(o =>
-        {
-            var songToUpdate = SelectedSong ?? throw new InvalidOperationException();
-            songToUpdate.Title = this.CurrentTitle;
-            songToUpdate.Artist = this.CurrentArtist;
-            this.songsManager.UpdateSong(songToUpdate);
-        }, o => this.selectedSong != null && !string.IsNullOrEmpty(this.currentTitle));
+    public ICommand UpdateCommand => new Command(o =>
+    {
+        var songToUpdate = SelectedSong ?? throw new InvalidOperationException();
+        songToUpdate.Title = this.CurrentTitle;
+        songToUpdate.Artist = this.CurrentArtist;
+        this.songsManager.UpdateSong(songToUpdate.Song);
+    }, o => this.selectedSong != null && !string.IsNullOrEmpty(this.currentTitle));
 
-        public ICommand DeleteCommand => new Command(o =>
+    public ICommand DeleteCommand => new Command(o =>
+    {
+        if (this.SelectedSong is SongItemViewModel currentSongViewModel)
         {
-            if (this.SelectedSong is Song currentSong)
-            {
-                this.songsManager.DeleteSong(currentSong);
-                this.SongsCollection.Remove(currentSong);
-                this.SelectedSong = null;
-            }
-        }, o => this.selectedSong != null);
-
-        public SongsEditorViewModel(SongsManager songsManager)
-        {
-            this.songsManager = songsManager;
-            var firstSong = songsManager.AddSong(new Song(0, "Sample song", "Sample artist"));
-            SongsCollection.Add(firstSong);
-            var secondSong = songsManager.AddSong(new Song(0, "Other song", "Other artist"));
-            SongsCollection.Add(secondSong);
-            this.SelectedSong = firstSong;
-            SelectedSongChangedCommand.Execute(firstSong);
+            this.songsManager.DeleteSong(currentSongViewModel.Song);
+            this.SongsCollection.Remove(currentSongViewModel);
+            this.SelectedSong = null;
         }
+    }, o => this.selectedSong != null);
 
-        public event PropertyChangedEventHandler? PropertyChanged;
+    public SongsEditorViewModel(SongsManager songsManager)
+    {
+        this.songsManager = songsManager;
+        var firstSong = songsManager.AddSong(new Song(0, "Sample song", "Sample artist"));
+        var firstSongViewModel = new SongItemViewModel(firstSong);
+        SongsCollection.Add(firstSongViewModel);
+        var secondSong = songsManager.AddSong(new Song(0, "Other song", "Other artist"));
+        var secondSongViewModel = new SongItemViewModel(secondSong);
+        SongsCollection.Add(secondSongViewModel);
+        this.SelectedSong = firstSongViewModel;
+        SelectedSongChangedCommand.Execute(firstSong);
+    }
 
-        public void OnPropertyChanged(string propertyName)
-        {
-            if (this.PropertyChanged != null)
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-        }
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public void OnPropertyChanged(string propertyName)
+    {
+        if (this.PropertyChanged != null)
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
     }
 }
